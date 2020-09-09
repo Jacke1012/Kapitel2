9e61180fe2092c489628161a1c0d78977afd536e
Jacke1012 <46431999+Jacke1012@users.noreply.github.com>
2020-09-09 17:02:30 +0200

    Update MyTextBox.cs

--- Bank_Övning/MyTextBox.cs
+++ Bank_Övning/MyTextBox.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.ComponentModel;
+using System.Drawing.Design;
+using System.Runtime.InteropServices;
 
 namespace Bank_Övning
 {
