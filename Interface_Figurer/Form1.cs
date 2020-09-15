3bbf1dc3b83fba40e8d042ec2b45c734a7b7c3f4
Jacke1012 <46431999+Jacke1012@users.noreply.github.com>
2020-09-15 12:29:28 +0200

    name spaces

--- Interface_Figurer/Form1.cs
+++ Interface_Figurer/Form1.cs
@@ -1,15 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Runtime;
-using System.Dynamic;
-using Microsoft.VisualBasic.CompilerServices;
 
 namespace Interface_Figurer
 {
