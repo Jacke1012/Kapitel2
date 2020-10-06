a38461ce5ca586dfc31263b62ce3f25d30a5e082
Jacob Jonasson <jacob.jonasson2003@gmail.com>
2020-10-06 15:20:26 +0200

    MyButton creation

---
+++ Bank_Övning/MyButton.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.ComponentModel;
+
+namespace Bank_Övning
+{
+    class MyButton : Button
+    {
+        [Category("Data"), Description("Description"),DefaultValue("")]
+        public string description { get; set; }
+        
+    }
+}
